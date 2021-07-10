92de8c46e79002f7bb6874abdbed1f99b22c4ccf
BliTz <60670940+BliTz037@users.noreply.github.com>
2021-07-10 18:54:29 +0200

    [EDIT] router name

--- src/routes/auth/auth.js
+++ src/routes/auth/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authCtrl = require('./auth.query');
 
-router.post('/signup', authCtrl.signup);
+router.post('/register', authCtrl.signup);
 router.post('/login', authCtrl.login);
 
 module.exports = router;
