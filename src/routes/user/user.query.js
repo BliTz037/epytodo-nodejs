aecc08590d51c69d99bcff69b9b7a692551fc26e
Tom Rives <tom.rives@epitech.eu>
2021-07-17 22:01:51 +0200

    [ADD][user] getUserInfo

--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -23,7 +23,20 @@ exports.getUserTodos = (req, res, next) => {
 }
 
 exports.getUserInfo = (req, res, next) => {
-    res.status(200).json({msg: "OK"});
+    let sql = `SELECT * FROM user WHERE '${req.params.id}' IN(id, email)`;
+
+    console.log(req.params);
+    if (!req.params.hasOwnProperty('id'))
+        return res.status(400).json({msg: "Missing parameter"});
+    db.query(sql, function(err, result) {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ msg: "internal server error" });
+        }
+        if (result.length === 0)
+            return res.status(404).json({msg: "User not found"});
+        res.status(200).json(result);
+    })
 }
 
 exports.updateUser = (req, res, next) => {
