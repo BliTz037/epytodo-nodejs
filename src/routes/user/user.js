712eac988d108550ffeb5e0bfa9483ebcb508bba
Tom Rives <tom.rives@epitech.eu>
2021-07-17 15:25:41 +0200

    [ADD][User] getAllUsersInfo

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -4,10 +4,10 @@ const auth = require('../../middleware/auth');
 const userCtrl = require('./user.query');
 
 router.get('/', auth, userCtrl.getAllUsersInfo);
-// router.get('/todos', auth, userCtrl.getUserTodos);
-// router.get('/:id', auth, userCtrl.getUserInfo);
-// router.get('/:email', auth, userCtrl.getUserInfo);
-// router.put('/:id ', auth, userCtrl.updateUser);
-// router.delete("/:id", auth, userCtrl.deleteUser);
+router.get('/todos', auth, userCtrl.getUserTodos);
+router.get('/:id', auth, userCtrl.getUserInfo);
+router.get('/:email', auth, userCtrl.getUserInfo);
+router.put('/:id ', auth, userCtrl.updateUser);
+router.delete("/:id", auth, userCtrl.deleteUser);
 
 module.exports = router;
--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -1,5 +1,28 @@
 const db = require('../../config/db').connection;
 
 exports.getAllUsersInfo = (req, res, next) => {
+    const sql = `SELECT * FROM user`;
+    db.query(sql, function(err, results) {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ msg: "internal server error" });
+        }
+        res.status(200).json(results);
+    })
+}
+
+exports.getUserTodos = (req, res, next) => {
+    res.status(200).json({msg: "OK"});
+}
+
+exports.getUserInfo = (req, res, next) => {
+    res.status(200).json({msg: "OK"});
+}
+
+exports.updateUser = (req, res, next) => {
+    res.status(200).json({msg: "OK"});
+}
+
+exports.deleteUser = (req, res, next) => {
     res.status(200).json({msg: "OK"});
 }
