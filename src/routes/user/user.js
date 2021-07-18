d3d0dbaa9a879d73be34d4f2df6d0af854b09ee4
Tom Rives <tom.rives@epitech.eu>
2021-07-18 14:28:18 +0200

    [ADD][User] Update user

--- src/routes/user/user.js
+++ src/routes/user/user.js
@@ -7,7 +7,7 @@ router.get('/', auth, userCtrl.getAllUsersInfo);
 router.get('/todos', auth, userCtrl.getUserTodos);
 router.get('/:id', auth, userCtrl.getUserInfo);
 router.get('/:email', auth, userCtrl.getUserInfo);
-router.put('/:id ', auth, userCtrl.updateUser);
+router.put('/:id', auth, userCtrl.updateUser);
 router.delete("/:id", auth, userCtrl.deleteUser);
 
 module.exports = router;
--- src/routes/user/user.query.js
+++ src/routes/user/user.query.js
@@ -1,4 +1,5 @@
 const db = require('../../config/db').connection;
+const bcrypt = require('bcrypt');
 
 exports.getAllUsersInfo = (req, res, next) => {
     const sql = `SELECT * FROM user`;
@@ -40,7 +41,35 @@ exports.getUserInfo = (req, res, next) => {
 }
 
 exports.updateUser = (req, res, next) => {
-    res.status(200).json({msg: "OK"});
+    if (!req.params.hasOwnProperty('id'))
+        return res.status(400).json({msg: "Missing parameter"});
+        console.log(req.body);
+    if (!req.body.hasOwnProperty("email") ||
+        !req.body.hasOwnProperty("password") ||
+        !req.body.hasOwnProperty("firstname") ||
+        !req.body.hasOwnProperty("name"))
+        return res.status(400).json({msg: "Missing parameter"});
+    
+    bcrypt.hash(req.body.password, 10)
+    .then(hash => {
+        const sql = `UPDATE user SET email = '${req.body.email}', password = '${hash}', firstname = '${req.body.firstname}', name = '${req.body.name}' WHERE id = '${req.params.id}'`;
+        db.query(sql, function(err, result) {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ msg: "internal server error" });
+            }
+            if (result.affectedRows === 0)
+                return res.status(404).json({ msg: "User not found"});
+            db.query(`SELECT * FROM user WHERE id = '${req.params.id}'`, function(err, result) {
+                if (err) {
+                    console.error(err);
+                    return res.status(500).json({ msg: "internal server error" });
+                }
+                res.status(200).json(result);
+            })
+        })
+    })
+    .catch(error => res.status(500).json({ error }));
 }
 
 exports.deleteUser = (req, res, next) => {
